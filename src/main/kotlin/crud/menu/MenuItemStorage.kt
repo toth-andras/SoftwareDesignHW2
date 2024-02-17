85f0f6f4ccd3fdd672bcf0eb62dfe7fe9f6234ac
totandras <83181732+toth-andras@users.noreply.github.com>
2024-02-17 15:41:28 +0300

    Finished interface for MenuItem storage.

--- src/main/kotlin/crud/menu/MenuItemStorage.kt
+++ src/main/kotlin/crud/menu/MenuItemStorage.kt
@@ -8,6 +8,22 @@ import org.example.entities.menu.MenuItem
  */
 interface MenuItemStorage: Storage {
     /**
+     * Создает блюдо с переданными данными и сохраняет его в хранилище.
+     * @param name название блюда.
+     * @param quantity количество порций блюда.
+     * @param price цена за одну порцию блюда.
+     * @param timeToCook время преготовления одной порции блюда.
+     * @return блюдо, добавленное в хранилище.
+     */
+    fun createMenuItem(name: String, quantity: Int, price: Int, timeToCook: Int): MenuItem
+
+    /**
+     * Добавляет в хранилище уже существующее блюдо, переопределяя его id.
+     * @param menuItem блюдо, которое необходимо доаавить в хранилище.
+     */
+    fun addMenuItem(menuItem: MenuItem)
+
+    /**
      * Получает из хранилища все имеющиеся блюда.
      * @return коллекцию блюд из хранилища.
      */
@@ -20,4 +36,10 @@ interface MenuItemStorage: Storage {
      */
     fun getMenuItem(id: Int): MenuItem?
 
+    /**
+     * Удаляет из хранилища блюдо с переданным id.
+     * @param id id блюда, которое необходимо удалить.
+     * @return true, если блюдо с переданным id было удалено и false иначе.
+     */
+    fun removeMenuItem(id: Int): Boolean
 }
