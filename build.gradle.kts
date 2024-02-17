7fc20d2dbbda3fe57d1290ac9e907e5fec3d3f29
totandras <83181732+toth-andras@users.noreply.github.com>
2024-02-17 14:47:03 +0300

    Added basic auth entities

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 group = "org.example"
@@ -11,6 +12,8 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
 }
 
 tasks.test {
---
+++ src/main/kotlin/entities/auth/Session.kt
@@ -0,0 +1,52 @@
+package org.example.entities.auth
+
+import kotlinx.datetime.*
+
+/**
+ * Хранит информацию об авторизованном в данный момент пользователе (при наличии) и дате авторизации.
+ */
+class Session {
+    private var _user: User? = null
+    private var _dateOfAuthorization: LocalDateTime? = null
+
+    /**
+     * Текущий авторизованный пользователь или null, если ни один пользователь не авторизован.
+     */
+    var user: User?
+        get() = _user
+        private set(value) {
+            _user = value
+        }
+
+    /**
+     * Дата авторизации текущего пользователя или null, если ни один пользователь не авторизован.
+     */
+    var dateOfAuthorization: LocalDateTime?
+        get() = _dateOfAuthorization
+        private set(value) {
+            _dateOfAuthorization = value
+        }
+
+    /**
+     * Авторизовать пользователя.
+     * @param userToAuthorize пользователь, которого необходимо авторизовать.
+     * @param password пароль пользователя, которого необходимо авторизовать.
+     * @return true, если авторизация пользователя произведена и false иначе.
+     */
+    fun authorizeUser(userToAuthorize: User, password: String): Boolean {
+        if (!userToAuthorize.checkPassword(password)) {
+            return false
+        }
+        user = userToAuthorize
+        dateOfAuthorization = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        return true
+    }
+
+    /**
+     * Если какой-либо пользователь авторизован, исключает его из сессии.
+     */
+    fun deAuthorizeUser(){
+        user = null
+        dateOfAuthorization = null
+    }
+}
