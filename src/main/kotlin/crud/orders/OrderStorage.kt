c8c5716b5f4a07e72d63406012ac23608826c408
totandras <83181732+toth-andras@users.noreply.github.com>
2024-02-18 12:05:15 +0300

    Added crud logic to orders.

---
+++ src/main/kotlin/crud/orders/OrderStorage.kt
@@ -0,0 +1,46 @@
+package org.example.crud.orders
+
+import org.example.crud.Storage
+import org.example.entities.auth.User
+import org.example.entities.menu.MenuItem
+import org.example.entities.orders.Order
+
+/**
+ * Представляет обобщенный интерфейс хранилища заказов.
+ */
+interface OrderStorage: Storage {
+    /**
+     * Возвращает все заказы, которые есть в хранилище.
+     * @return коллекцию заказов.
+     */
+    fun getOrders(): Iterable<Order>
+
+    /**
+     * Возвращает заказ с переданным идентификатором.
+     * @param id идентификатор заказа.
+     * @return заказ с переданным идентифкатором или null,
+     * если заказа с таким идентификатором нет в хранилище.
+     */
+    fun getOrder(id: Int): Order?
+
+    /**
+     * Создает новый заказ и добавляет его в хранилище.
+     * @param user посетитель, оформивший заказ.
+     * @param menuItems блюда, которые заказал посетитель.
+     * @return созданный заказ.
+     */
+    fun createOrder(user: User, menuItems: List<MenuItem>): Order
+
+    /**
+     * Добавляет заказ в хранилище.
+     * @param order заказ.
+     */
+    fun addOrder(order: Order)
+
+    /**
+     * Удаляет заказ с переданным идентификатором из хранилища.
+     * @param id идентификатор заказа.
+     * @return true, если заказ с переданным идентификатором был удален и false иначе.
+     */
+    fun removeOrder(id: Int): Boolean
+}
---
+++ src/main/kotlin/crud/orders/OrderStorageJson.kt
@@ -0,0 +1,76 @@
+package org.example.crud.orders
+
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.example.entities.auth.User
+import org.example.entities.menu.MenuItem
+import org.example.entities.orders.Order
+import java.io.File
+import java.io.IOException
+
+/**
+ * Представляет хранилище блюд, хранящее данные в формате json.
+ * @param sourcePath путь к файлу в формате .json, в котором хранятся данные о заказах.
+ */
+class OrderStorageJson(private val sourcePath: String): OrderStorage {
+    private var _orders: MutableMap<Int, Order> = mutableMapOf()
+    private var _nextId = 0
+
+    override fun initialize() {
+        _orders = try {
+            Json.decodeFromString(File(sourcePath).readText())
+        } catch (e: IOException) {
+            mutableMapOf()
+        }
+        catch (e: SerializationException) {
+            mutableMapOf()
+        }
+
+        if (_orders.any()) {
+            _nextId = _orders.maxOf { it.value.id } + 1
+        }
+    }
+
+    override fun destruct() {
+        save()
+    }
+
+    override fun getOrders(): Iterable<Order> = _orders.values
+
+    override fun getOrder(id: Int): Order? = _orders[id]
+
+    override fun createOrder(user: User, menuItems: List<MenuItem>): Order {
+        val order = Order(_nextId++, user, menuItems)
+        _orders[order.id] = order
+
+        return order
+    }
+
+    override fun removeOrder(id: Int): Boolean {
+        if (!_orders.containsKey(id)) {
+            return false
+        }
+
+        _orders.remove(id)
+        return true
+    }
+
+    override fun addOrder(order: Order) {
+        order.id = _nextId++
+        _orders[order.id] = order
+    }
+
+    private fun save() {
+        try {
+            File(sourcePath).writeText(Json.encodeToString(_orders))
+        }
+        // В текущей реализации я решил, что объект просто будет игнорировать ошибку.
+        catch (e: IOException) {
+            return
+        }
+        catch (e: SerializationException) {
+            return
+        }
+    }
+}
