3a35a8d8fa2c3036cc38c5eeb036379e6cfb6129
totandras <83181732+toth-andras@users.noreply.github.com>
2024-02-20 16:23:50 +0300

    Added purple color to StringColorizer.

--- src/main/kotlin/utils/StringColorizer.kt
+++ src/main/kotlin/utils/StringColorizer.kt
@@ -32,9 +32,15 @@ class StringColorizer {
         fun toBlue(string: String) = "\u001b[34m$string$colorReset"
 
         /**
-         * Окрасить белый в синий цвет.
+         * Окрасить строку в белый цвет.
          * @param string строка.
          */
         fun toWhite(string: String) = "\u001b[37m$string$colorReset"
+
+        /**
+         * Окрасить строку в фиолетовый цвет.
+         * @param string строка.
+         */
+        fun toPurple(string: String) = "\u001b[35m$string$colorReset"
     }
 }
