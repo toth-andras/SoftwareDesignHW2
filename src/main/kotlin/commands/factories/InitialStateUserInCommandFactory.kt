0d7d77c59180d9f465d39fac959738de109a567c
totandras <83181732+toth-andras@users.noreply.github.com>
2024-02-21 09:38:41 +0300

    fixed #5

--- src/main/kotlin/commands/factories/InitialStateUserInCommandFactory.kt
+++ src/main/kotlin/commands/factories/InitialStateUserInCommandFactory.kt
@@ -6,12 +6,13 @@ import org.example.commands.auth.DeleteAccountCommand
 import org.example.commands.auth.ExitCommand
 import org.example.commands.auth.LogoutCommand
 import org.example.commands.auth.RegisterCommand
+import org.example.commands.navigation.MainMenuCommand
 
 /**
  * Создает команды, доступные в начальном состоянии приложения при условии, что пользователь авторизован.
  */
 class InitialStateUserInCommandFactory: CommandFactory<Application> {
     override fun createCommandSet(): Iterable<Command<Application>> {
-        return listOf(LogoutCommand(), DeleteAccountCommand(), ExitCommand())
+        return listOf(MainMenuCommand(), LogoutCommand(), DeleteAccountCommand(), ExitCommand())
     }
 }
